7357817925de29ea637b97af24a7b3cdf6075265
Rafael Marques <rafaelmarques.rm86@gmail.com>
2023-06-16 13:10:41 -0300

    Consumindo API no Console

--- CosumidorConsole/Program.cs
+++ CosumidorConsole/Program.cs
@@ -1,5 +1,27 @@
-﻿//Gerar uma tarefa
+﻿using CosumidorConsole;
+using Flurl.Http;
 
+string url = "https://localhost:7182/";
+
+Item tarefa1 = new Item();
+tarefa1.Id = 1;
+tarefa1.Nome = "Pagar conta";
+tarefa1.Finalizado = true;
+
+Item tarefa2 = new Item();
+tarefa2.Id = 2;
+tarefa2.Nome = "Estudar";
+tarefa2.Finalizado = false;
+
+//Gerar uma tarefa
+string endpoint = url + "api/TarefaItems";
+
+//retarda a chama e esperar a nossa api estar no ar
+Thread.Sleep(new TimeSpan(0, 0, 5));
+
+//flurl
+endpoint.PostJsonAsync(tarefa1);
+endpoint.PostJsonAsync(tarefa2);
 //Ler a lista
 
 //Altera
