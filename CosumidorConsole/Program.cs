02f1aba40d33ab85e173e67f00e7e6edb0aa06c1
Rafael Marques <rafaelmarques.rm86@gmail.com>
2023-06-16 12:49:14 -0300

    Criando projetos para consumir a API

---
+++ CosumidorConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿//Gerar uma tarefa
+
+//Ler a lista
+
+//Altera
+
+//Ler a lista
+
+//Deletar um item da lista
+
+//Ler a lista
+
+Console.WriteLine("Aperte Qualquer tecla para finalizar a aplicação");
+Console.Read();
