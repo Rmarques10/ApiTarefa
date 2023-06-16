e2221ab42d064b12397bb11c6633f5732629a557
Rafael Marques <rafaelmarques.rm86@gmail.com>
2023-06-16 10:03:29 -0300

    Criando o contexto

---
+++ Api_Tarefa/Context/TarefaContext.cs
@@ -0,0 +1,15 @@
+﻿using Api_Tarefa.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api_Tarefa.Context
+{
+    public class TarefaContext : DbContext
+    {
+        public TarefaContext(DbContextOptions<TarefaContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TarefaItem> Tarefas { get; set; }
+    }
+}
